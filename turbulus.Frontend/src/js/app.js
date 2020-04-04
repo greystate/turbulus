6955d842b4aaf5894203e8106b2c554acb3fed4a
Chriztian Steinmeier <chriztian@steinmeier.dk>
2020-04-05 00:17:36 +0200

    Add a simple test
    The console should only get a single message from the initial response

--- turbulus.Frontend/src/js/app.js
+++ turbulus.Frontend/src/js/app.js
@@ -1,11 +1,3 @@
-/* A demo Point to test the build */
-class Point {
-	constructor(x = 0, y = 0) {
-		this.x = x
-		this.y = y
-	}
-	
-	draw() {
-		return `[${this.x}, ${this.y}]`
-	}
-}
+// This is to verify that Turbolinks takes over.
+// This will only run on the initial response from the server.
+document.addEventListener('DOMContentLoaded', (event) => console.log('DOMContentLoaded'))
