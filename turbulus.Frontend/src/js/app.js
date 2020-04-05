5036fe50e5a489ef4ae0ebd5e1f6fd1b0c2d1fd9
Chriztian Steinmeier <chriztian@steinmeier.dk>
2020-04-05 13:44:21 +0200

    Add a Stimulus controller
    Includes some debug logging

--- turbulus.Frontend/src/js/app.js
+++ turbulus.Frontend/src/js/app.js
@@ -1,3 +1,42 @@
-// This is to verify that Turbolinks takes over.
-// This will only run on the initial response from the server.
-document.addEventListener('DOMContentLoaded', (event) => console.log('DOMContentLoaded'))
+
+class Movies extends Stimulus.Controller {
+	static get targets() {
+		return ['filter', 'list']
+	}
+	
+	initialize() {
+		const me = this
+		document.addEventListener('turbolinks:before-visit', event => me.fadeOutList())
+		document.addEventListener('turbolinks:before-render', me.hideList)
+		document.addEventListener('turbolinks:render', console.log('render'))
+	}
+	
+	connect() {
+		console.log('connecting...')
+		const me = this
+		setTimeout(() => me.fadeInList(), 10)
+	}
+
+	fadeOutList() {
+		console.log('fading...')
+		this.listTarget.classList.add('fade')
+	}
+	
+	fadeInList() {
+		console.log('unfading...')
+		this.listTarget.classList.remove('fade')
+	}
+	
+	hideList(event) {
+		let newBody = event.data.newBody
+		newBody.querySelector('.movielist').classList.add('fade')
+		event.data.newBody = newBody
+		console.log(newBody.querySelector('.movielist').className)
+	}
+}
+
+
+const app = Stimulus.Application.start()
+
+app.register('movies', Movies)
+
--- turbulus.Frontend/src/js/libs.js
+++ turbulus.Frontend/src/js/libs.js
@@ -1,2 +1,3 @@
 /* Use this for including any vendor libraries */
 // @codekit-append "../../vendor/turbolinks/turbolinks.js"
+// @codekit-append "../../vendor/stimulus/stimulus.umd.js"
